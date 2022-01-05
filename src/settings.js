96a9db4ce73705c5ea87f3da11bfc0126868acbe
zhouxudong666 <39668020+zhouxudong666@users.noreply.github.com>
2022-01-05 13:48:43 +0800

    Add files via upload

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","move","UI","fish"],collisionMatrix:[[true,true,true,true],[true,true,null,true],[true,false,false],[true,true,false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/MainGameScene.fire",orientation:"portrait",jsList:[]};
